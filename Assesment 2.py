af090da8bd0f45337a5a5442ebc0fb812ecbfb75
Jess <2313453@live.stmarys.ac.uk>
2024-07-07 18:21:01 +0100

    start

--- Assesment 2.py
+++ Assesment 2.py
@@ -2,7 +2,6 @@ import sqlite3
 import hashlib
 
 
-
 def connect_db():
     return sqlite3.connect('stmarys_logistics.db')
 
@@ -17,6 +16,45 @@ def setup_database():
         location TEXT NOT NULL,
         capacity INTEGER NOT NULL,
         ''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Inventory (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        warehouse_id INTEGER NOT NULL,
+        item_name TEXT NOT NULL,
+        quantity INTEGER NOT NULL,
+        threshhold INTEGER NOT NULL,
+        FOREIGN KEY(warehouse_id) REFERENCES Warehouses(id),
+        ''')
+
+        cursor.execute('''
+               CREATE TABLE IF NOT EXISTS Transportation (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   vehicle_id TEXT NOT NULL,
+                   driver_name TEXT NOT NULL,
+                   schedule TEXT NOT NULL,
+                   route TEXT NOT NULL
+               ''')
+
+        cursor.execute('''
+              CREATE TABLE IF NOT EXISTS Users (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  username TEXT UNIQUE NOT NULL,
+                  password TEXT NOT NULL,
+                  role TEXT NOT NULL
+              ''')
+
+        print("Database setup complete.")
+
+
+
+
+
+
+
+
+
+
 
 
 
